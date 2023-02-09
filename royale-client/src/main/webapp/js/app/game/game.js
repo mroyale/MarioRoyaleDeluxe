09feb62074de17a06bcea5ad4b4d5e520e41e5df
terminalarch <terminalartix@mail.com>
2023-02-09 11:17:37 +0000

    Vertical camera centered, snaps to bottom

--- royale-client/src/main/webapp/js/app/game/game.js
+++ royale-client/src/main/webapp/js/app/game/game.js
@@ -690,9 +690,9 @@ Game.prototype.doStep = function() {
   var zone = this.getZone();
   if(ply && !ply.dead && !this.cameraLocked) {
     switch (zone.camera) {
-      case 0 : { this.display.camera.position(vec2.make(Math.max(15, ply.pos.x), zone.dimensions().y * .5)); break; }
-      case 1 : { this.display.camera.positionY(-ply.pos.y + zone.dimensions().y); break; }
-      case 2 : { this.display.camera.position(vec2.make(Math.max(15, ply.pos.x), -ply.pos.y + zone.dimensions().y)); break; }
+      case 0 : { this.display.camera.position(vec2.make(Math.max(15, ply.pos.x), zone.dimensions().y * .5)); break; } // Horizontal Scrolling
+      case 1 : { this.display.camera.positionX(zone.dimensions().x*.5); this.display.camera.positionY(Math.min(9, -ply.pos.y + zone.dimensions().y)); break; } // Vertical Scrolling (horizontal is always centered)
+      case 2 : { this.display.camera.position(vec2.make(Math.max(15, ply.pos.x), Math.min(9, -ply.pos.y + zone.dimensions().y))); break; } // Free Roam (horizontal and vertical)
     }
   }
   
