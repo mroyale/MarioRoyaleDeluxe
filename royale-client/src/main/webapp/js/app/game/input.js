2228a1e5ea76a2428e8fb26fb65f62fcf29b33de
terminalarch <terminalartix@mail.com>
2023-03-07 08:14:15 +0100

    controls fix

--- royale-client/src/main/webapp/js/app/game/input.js
+++ royale-client/src/main/webapp/js/app/game/input.js
@@ -14,8 +14,8 @@ function Input(game, container) {
   this.container.onmouseup = function(event) { that.mouse.event(event, false); };
   this.container.addEventListener("mousewheel", function(event) { that.mouse.wheel(event); }, false); // IE9, Chrome, Safari, Opera
   this.container.addEventListener("DOMMouseScroll", function(event) { that.mouse.wheel(event); }, false); // Firefox
-  document.onkeyup = function(event) { that.keyboard.event(event, false); };
-  document.onkeydown = function(event) { that.keyboard.event(event, true); };
+  document.onkeyup = function(event) { that.keyboard.event(event, false); controlApp.keyEvent(event, false); };
+  document.onkeydown = function(event) { that.keyboard.event(event, true); controlApp.keyEvent(event, true); };
 
   this.touchEvt = function(event) { app.game.input.touch.event(event); };
   
